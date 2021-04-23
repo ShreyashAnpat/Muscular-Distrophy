40218934a52080b813f062238533c6027ea6da06
Shreyash Anpat <anpatshreyash@gmail.com>
2021-04-23 22:24:41 +0530

    first commit

---
+++ app/src/main/java/com/example/musculardistrophy/Flash_Screen.java
@@ -0,0 +1,14 @@
+package com.example.musculardistrophy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Flash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash__screen);
+    }
+}
