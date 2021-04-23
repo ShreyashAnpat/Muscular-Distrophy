f9576fe2a9e85c1579d9e6e968910d9f1c22c6ae
Shreyash Anpat <anpatshreyash@gmail.com>
2021-04-23 23:43:25 +0530

    flash screen added

--- app/src/main/java/com/example/musculardistrophy/Flash_Screen.java
+++ app/src/main/java/com/example/musculardistrophy/Flash_Screen.java
@@ -2,7 +2,14 @@ package com.example.musculardistrophy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.musculardistrophy.Login.Login_Activity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Flash_Screen extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class Flash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash__screen);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Flash_Screen.this, Login_Activity.class));
+                finish();
+            }
+        },4000);
+
     }
 }
---
+++ app/src/main/java/com/example/musculardistrophy/Login/Login_Activity.java
@@ -0,0 +1,18 @@
+package com.example.musculardistrophy.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.musculardistrophy.R;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+
+
+    }
+}
